8219a5cb0a9b9cbf8a4c45bff6c4fcf9cc54f2f3
Leoprog23 <you@example.com>
2023-11-23 20:23:41 -0300

    mudança no banco postgresql

--- FakePinterest/__init__.py
+++ FakePinterest/__init__.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 # Criando o site, a aplicação:
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db" # Criando um banco de dados
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")# Pegando o link do banco de dados virtual utilizado pelo Render
 app.config["SECRET_KEY"] = "df406478193049f187564a3a4da5f8dc"
 app.config["UPLOAD_FOLDER"] = "static/Fotos_posts"
 
